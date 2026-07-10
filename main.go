c793e444af8546786de60a8152b77b7373a0e80f
oscar <oscar.luu@bit.ly>
2026-07-10 14:00:12 -0700

    tidepool test run

--- main.go
+++ main.go
@@ -14,6 +14,11 @@ func main() {
 
 	http.Handle("/", http.FileServer(http.Dir(".")))
 
+	// Serve the tic-tac-toe game at a clean URL.
+	http.HandleFunc("/tictactoe", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "tic-tac-toe.html")
+	})
+
 	addr := ":" + port
 	log.Printf("listening on %s", addr)
 	if err := http.ListenAndServe(addr, nil); err != nil {
